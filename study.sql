f123596df1316b9d6348ea753d266528633e8143
Neeraj Kumar <neerajmail888@gmail.com>
2025-08-28 14:06:52 +0530

    commit

--- study.sql
+++ study.sql
@@ -1,3 +1,5 @@
-/*    1. How many layoffs occurred in total across all companies? */
+-- 1. How many layoffs occurred in total across all companies?
 
-select 
+
+select sum(total_laid_off) as 'Total Layoffs Across all companies' from layoff;
+
